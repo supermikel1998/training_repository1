e052be02c2a43b83186dc4280f268b67da306fdd
Michael Lindsey Simborio <michael.l.d.simborio@accenture.com>
2025-02-19 07:39:23 +0000

    Activity 1

--- project1/webapp/controller/training1.controller.js
+++ project1/webapp/controller/training1.controller.js
@@ -2,9 +2,21 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ], (Controller) => {
     "use strict";
-
+    
     return Controller.extend("com.acn.project1.project1.controller.training1", {
         onInit() {
+
+        },
+
+        onClickClear: function() {
+            this.byId("txtName").setValue("");
+            this.byId("txtStreet").setValue("");
+            this.byId("txtNo").setValue("");
+            this.byId("txtZip").setValue("");
+            this.byId("txtCity").setValue("");
+            this.byId("ddCountry").setValue("");
         }
+
     });
 });
+
